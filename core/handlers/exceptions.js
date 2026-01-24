50cc51a8e8d6c1fe7d938731bd4d2642a79741ad
20044kIROS <aldshnkansama@gmail.com>
2026-01-24 13:03:54 +0200

    Update exceptions.js

--- core/handlers/exceptions.js
+++ core/handlers/exceptions.js
@@ -1,15 +1,29 @@
-const ErrorName = {
-    UNEXPECTED: "UnexpectedError",
-    USER_NOT_EXISTS: "UserNotFoundError",
-};
-
-export class BalanceError extends Error {
-    /**
-     * @param {keyof ErrorName} name
-     * @param {string} message 
-     */
-    constructor(name, message) {
-        super(message);
-        this.name = name;
-    }
+{
+  "APPSTATE_PATH": "./appstate.json",
+  "APPSTATE_PROTECTION": true,
+  "AUTO_PING_SERVER": true,
+  "ALLOW_INBOX": false,
+  "DATABASE": "JSON",
+  "DATABASE_JSON_BEAUTIFY": true,
+  "FCA_OPTIONS": {
+    "pauseLog": true,
+    "logLevel": "silent",
+    "selfListen": false,
+    "listenEvents": true,
+    "forceLogin": true,
+    "userAgent": "Mozilla/5.0 (Li"
+  },
+  "LANGUAGE": "ar_YS",
+  "LOG_LEVEL": 1,
+  "MODERATORS": [
+    "100054677207079"
+  ],
+  "ABSOLUTES": [
+    "100054677207079"
+  ],
+  "NAME": "ᏆᎬᏁᎶᎬᏁ ᏚᎯᎷᎯ",
+  "PREFIX": "-",
+  "TIMEZONE": "Asia/Ho_Chi_Minh",
+  "REFRESH": "4320000",
+  "MAX_BALANCE_LIMIT": "-1"
 }
